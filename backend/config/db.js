3ef16ab0f44495820f56b3b68f4db959458d05d3
Soumyata12 <soumyata9sarkar@gmail.com>
2026-01-12 16:51:55 +0530

    Changes to db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize("ride_sharing_db", "root", "Diya_2003", {
+const sequelize = new Sequelize("ride_sharing_db", "root", "", {
   host: "localhost",
   dialect: "mysql",
   logging: false,
