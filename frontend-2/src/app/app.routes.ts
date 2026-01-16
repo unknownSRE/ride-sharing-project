3aa0f42b8bead590a0ce380ab257d309b33b1ba1
unknownSRE <hitxpert@gmail.com>
2026-01-16 12:57:02 +0530

    Merge and resolve conflicts in app.routes.ts

--- frontend-2/src/app/app.routes.ts
+++ frontend-2/src/app/app.routes.ts
@@ -1,23 +1,15 @@
-// src/app/app.routes.ts
+// frontend-2/src/app/app.routes.ts
 import { Routes } from '@angular/router';
-<<<<<<< HEAD
 import { UserAuthComponent } from './components/users/users';
-
-export const routes: Routes = [
-  { path: 'auth', component: UserAuthComponent },
-  { path: '', redirectTo: 'auth', pathMatch: 'full' }, // Redirect default path to login
-  { path: '**', redirectTo: 'auth' } // Wildcard route for 404s
-];
-=======
 import { RatingComponent } from './components/rating/rating';
 import { PaymentComponent } from './components/payment/payment';
 
 export const routes: Routes = [
+  { path: 'auth', component: UserAuthComponent },
   { path: 'rate', component: RatingComponent },
   { path: 'payment', component: PaymentComponent },
 
-  // optional: make / land on /payment for now:
-  { path: '', redirectTo: 'payment', pathMatch: 'full' },
-  { path: '**', redirectTo: 'payment' }
+  // Set default redirect (choose which one should be the landing page)
+  { path: '', redirectTo: 'auth', pathMatch: 'full' },
+  { path: '**', redirectTo: 'auth' },
 ];
->>>>>>> e36b206854647ef99048640b37e42184ee96a223
